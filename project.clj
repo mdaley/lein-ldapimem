606e0b944af057cfd5e61ce98fc24e8beba94d24
Matthew Daley <matt.daley@blueyonder.co.uk>
2015-03-31 21:11:51 +0100

    repositories described.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ldapimem "0.1.0"
+(defproject lein-ldapimem "0.1.0-SNAPSHOT"
   :description "Creates an in memory LDAP server based on UnboundID's implementation"
   :url "http://github.com/mdaley/lein-ldapimem"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,6 @@
   :dependencies [[com.unboundid/unboundid-ldapsdk "2.3.8"]]
   :scm {:name "git"
         :url "https://github.com/mdaley/lein-ldapimem"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]])
