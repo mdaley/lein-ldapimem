93220160f80281f26863a12e336440788ad449c4
Matthew Daley <matt.daley@blueyonder.co.uk>
2015-03-31 21:19:03 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ldapimem "0.1.0"
+(defproject lein-ldapimem "0.1.1-SNAPSHOT"
   :description "Creates an in memory LDAP server based on UnboundID's implementation"
   :url "http://github.com/mdaley/lein-ldapimem"
   :license {:name "Eclipse Public License"
